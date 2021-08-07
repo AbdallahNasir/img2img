8c9fab8a077f988cbd1cd411bd6a6394d1dc389d
Abdallah Nasir <eng.abdullah.naser@gmail.com>
2021-08-07 14:28:46 +0300

    Fixed a naming mistake

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -4,18 +4,18 @@ from tensorflow.keras.models import Model
 
 
 class Img2Img(Model):
-    def __init__(self, kernel_size_list=[32, 16], input_shape=(1054, 1054, 3)):
+    def __init__(self, filters_count_list=[32, 16], input_shape=(1054, 1054, 3)):
         super(Img2Img, self).__init__()
         encoder_layers = [layers.Conv2D(
                 size, (3, 3), activation='relu', padding='same', strides=2)
-                for size in kernel_size_list]
+                for size in filters_count_list]
         self.encoder = tf.keras.Sequential(
             [layers.Input(shape=input_shape)] + encoder_layers)
 
         decoder_layers = [layers.Conv2DTranspose(
                 size, kernel_size=3, strides=2,
                 activation='relu', padding='same')
-                for size in kernel_size_list]
+                for size in filters_count_list]
         decoder_layers.reverse()
         self.decoder = tf.keras.Sequential(decoder_layers + [
             layers.Conv2D(
